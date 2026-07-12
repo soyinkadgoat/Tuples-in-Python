a49c7dbe93fb478e1febd3b60e18941013dea919
soyinkadgoat <soyinkadgreat807@gmail.com>
2026-07-12 13:59:56 +0100

    Add files via upload

---
+++ tupleoperations.py
@@ -0,0 +1,16 @@
+tuples = (35, "boat", "chair", 2.7, 31, False, "exam")
+print(tuples)
+
+int_tuple = (30, 25, 50, 100, 3, 15, 44)
+print(int_tuple)
+
+added_tuple = int_tuple + (9, 0)
+print(added_tuple)
+
+occuring_tuple = ("ball", "boat", "bottom", "ball", "bird")
+
+print(occuring_tuple.count("ball"))
+
+slice_tuple = ("75", "egg", "flower", "36", "bottle")
+sliced_tuple = slice_tuple[0:2]
+print(sliced_tuple)
